e1935fbd3c77b5b2a595665211bd72825f6facce
Tim Watson <watson.timothy@gmail.com>
2011-11-21 10:18:27 +0000

    Merge rebar_cmd_builder into rebar_plugin_manager

--- src/rebar_plugin_manager.erl
+++ src/rebar_plugin_manager.erl
@@ -24,34 +24,57 @@
          command_info/1,
          plugins/1,
          once/2,
-         do_in_deps_dir/2]).
+         do_in_deps_dir/2,
+         generate_handler/3]).
 
+-define(DEBUG(Msg, Args), ?LOG(debug, Msg, Args)).
 -define(INFO(Msg), ?INFO(Msg, [])).
 -define(INFO(Msg, Args), ?LOG(info, Msg, Args)).
+-define(WARN(Msg, Args), ?LOG(warn, Msg, Args)).
 -define(LOG(Level, Msg, Args), rebar_log:log(Level, Msg, Args)).
 -define(ABORT(Msg, Args), rebar_utils:abort(Msg, Args)).
 
+-type command_name() :: atom().
+-type command_data() :: any().
+-type instruction_set() :: list(command_data()).
+-type command() :: {'command',
+                    command_name(),
+                    command_data(),
+                    instruction_set()}.
+
+-export_type([command_name/0,
+              command_data/0,
+              instruction_set/0,
+              command/0]).
+
+-spec preprocess(rebar_config:config(), string()) -> {ok, list()}.
 preprocess(Config, _) ->
     Plugins = plugins(Config),
     ?INFO("Managing Plugins: ~p~n", [Plugins]),
     update_code_path(Config),
     {ok, []}.
 
+-spec command_info(atom()) -> atom() | list(atom()).
 command_info(What) ->
     case erlang:function_exported(rebar_utils, command_info, 1) of
         true ->
             rebar_utils:command_info(What);
         false ->
-            {'EXIT', {Error, CallStack}} = (catch 1 = 2),
-            {M, _, _} = erlang:hd(erlang:tl(CallStack)),
-            ?ABORT("Plugin ~p requires a version of rebar that "
-                   "supports `rebar_utils:command_info/1'~n"
-                   "Please upgrade to a supported version.~n", [M])
+            V = 1,
+            case (catch V = undefined) of
+                {'EXIT', {_, CallStack}} ->
+                    {M, _, _} = erlang:hd(erlang:tl(CallStack)),
+                    ?ABORT("Plugin ~p requires a version of rebar that "
+                           "supports `rebar_utils:command_info/1'~n"
+                           "Please upgrade to a supported version.~n", [M])
+            end
     end.
 
+-spec plugins(rebar_config:config()) -> list(atom()).
 plugins(Config) ->
     lists:flatten(rebar_config:get_all(Config, plugins)).
 
+-spec once(atom(), fun(() -> any())) -> 'ok' | term().
 once(Tag, Command) ->
     case rebar_config:get_global(Tag, undefined) of
         undefined ->
@@ -62,6 +85,7 @@ once(Tag, Command) ->
             ok
     end.
 
+-spec do_in_deps_dir(string(), fun((string()) -> any())) -> 'ok'.
 do_in_deps_dir(DepsDir, Handler) ->
     case file:list_dir(DepsDir) of
         {ok, Dirs} ->
@@ -70,6 +94,7 @@ do_in_deps_dir(DepsDir, Handler) ->
             rebar_log:log(warn, "Unable to process deps: ~p~n", [Err])
     end.
 
+-spec deps_dir(rebar_config:config()) -> string().
 deps_dir(Config) ->
     case rebar_config:get_global(alt_deps_dir, undefined) of
         undefined ->
@@ -84,8 +109,116 @@ deps_dir(Config) ->
     end.
 
 %%
+%% @doc Generate '<Command>'(Config, _AppFile) handler functions for each
+%% command in `Cmds' and append them to either (a) a new module or (b) the
+%% origin (module) if (a) fails.
+%%
+-spec generate_handler(Base::string(), Cmds::list(rebar_cmd_builder:command()),
+                        Origin::module()) -> module().
+generate_handler(Base, Cmds, Origin) ->
+    case rebar_config:get_global({Base, Origin}, undefined) of
+        undefined ->
+            ?DEBUG("Generating handler(s) for ~p: ~p~n", [Base, Cmds]),
+            Exports = [ {C, 2} || {command, C, _, _} <- Cmds ],
+            Functions = [ gen_function(Base, C, Origin) ||
+                                            {command, C, _, _} <- Cmds ],
+
+            %% Using get_object_code can lead to all kinds of situations when
+            %% we're in an escript, when we're called by other code that isn't
+            %% and so on. This *mixed* approach is intended to cover the edge
+            %% cases I've seen thus far.
+            {Forms, Loader} = case code:get_object_code(Origin) of
+                {_,Bin,_} ->
+                    ?DEBUG("Compiling from existing binary~n", []),
+                    GeneratedForms = to_forms(atom_to_list(Origin),
+                                                Exports, Functions, Bin),
+                    {GeneratedForms, fun load_binary/2};
+                error ->
+                    File = code:which(Origin),
+                    case compile:file(File, [debug_info,
+                                             binary, return_errors]) of
+                        {ok, _, Bin} ->
+                            ?DEBUG("Compiling from binary~n", []),
+                            {to_forms(atom_to_list(Origin), Exports,
+                                    Functions, Bin), fun load_binary/2};
+                        Error ->
+                            ?WARN("Unable to recompile ~p: ~p~n",
+                                  [Origin, Error]),
+                            {mod_from_scratch(Base, Exports, Functions),
+                                    fun evil_load_binary/2}
+                    end;
+                _ ->
+                    ?WARN("Cannot modify ~p - generating new module~n",
+                          [Origin]),
+                    {mod_from_scratch(Base, Exports, Functions),
+                                                        fun evil_load_binary/2}
+            end,
+            Loaded = case compile:forms(Forms, [report, return]) of
+                {ok, ModName, Binary} ->
+                    Loader(ModName, Binary);
+                {ok, ModName, Binary, _Warnings} ->
+                    Loader(ModName, Binary);
+                CompileError ->
+                    ?ABORT("Unable to compile: ~p~n", [CompileError])
+            end,
+            rebar_config:set_global({Base, Origin}, Loaded),
+            Loaded;
+        Handler ->
+            Handler
+    end.
+
+%%
 %% Internal API
 %%
+
+mod_from_scratch(Base, Exports, Functions) ->
+    [{attribute, ?LINE, module, list_to_atom(Base ++
+                                             "_custom_build_plugin")},
+     {attribute, ?LINE, export, Exports}] ++ Functions.
+
+to_forms(Base, Exports, Functions, Bin) ->
+    case beam_lib:chunks(Bin, [abstract_code]) of
+        {ok, {_,[{abstract_code,{_,[FileDef, ModDef|Code]}}|_]}} ->
+            Code2 = lists:keydelete(eof, 1, Code),
+            [FileDef, ModDef] ++
+            [{attribute,31,export,Exports}] ++
+            Code2 ++ Functions; %%  ++ [EOF],
+        _ ->
+            [{attribute, ?LINE, module, list_to_atom(Base)},
+             {attribute, ?LINE, export, Exports}] ++ Functions
+    end.
+
+gen_function(Base, Cmd, Origin) ->
+    {function, ?LINE, Cmd, 2, [
+        {clause, ?LINE,
+            [{var,?LINE,'Config'},
+             {var,?LINE,'File'}],
+            [],
+            [{call,?LINE,
+                {remote,?LINE,
+                    {atom,?LINE,Origin},
+                    {atom,?LINE,execute_command}},
+                    [erl_parse:abstract(Cmd),
+                     erl_parse:abstract(Base),
+                     {var,?LINE,'Config'},
+                     {var,?LINE,'File'}]}]}]}.
+
+evil_load_binary(Name, Binary) ->
+    %% this is a nasty hack - perhaps adding the function to *this*
+    %% module would be a better approach, but for now....
+    ?DEBUG("Evil Loading: ~p~n", [Name]),
+    Name = load_binary(Name, Binary),
+    {ok, Existing} = application:get_env(rebar, any_dir_modules),
+    application:set_env(rebar, any_dir_modules, [Name|Existing]),
+    Name.
+
+load_binary(Name, Binary) ->
+    case code:load_binary(Name, "", Binary) of
+        {module, Name}  ->
+            ?DEBUG("Module ~p loaded~n", [Name]),
+            Name;
+        {error, Reason} -> ?ABORT("Unable to load binary: ~p~n", [Reason])
+    end.
 
 update_code_path(Config) ->
     once(setup_path,
